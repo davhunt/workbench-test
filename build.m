0432195567c52bfc09506b0cc2cf41945e98ade8
David Hunt <davhunt@indiana.edu>
2019-10-16 15:19:36 -0400

    find workbench

---
+++ build.m
@@ -0,0 +1,7 @@
+%addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+%addpath(genpath('/N/soft/mason/SPM/spm8'))
+addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
+addpath(genpath('/N/u/davhunt/Carbonate/Downloads/gifti-1.8'))
+mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
+exit
